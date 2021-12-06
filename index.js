6a7e4bce283c5493248d5baa0b4b269af1f873ab
sakamuuy <sakamuu.y@gmail.com>
2021-12-06 22:20:01 +0900

    Add babel

--- index.js
+++ index.js
@@ -65,12 +65,6 @@ const step0 = () => {
     render
   }
 
-  // const element = Didact.createElement(
-  //   'div',
-  //   { id: 'foo' },
-  //   Didact.createElement('a', null, 'bar'),
-  //   Didact.createElement('b')
-  // )
   /** @jsx Didact.createElement */
   const element = (
     <div id="foo">
