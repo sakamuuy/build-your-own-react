b525200c7405590710064e6e3bc88b0e5d73d687
sakamuuy <sakamuu.y@gmail.com>
2021-12-07 22:37:31 +0900

    step6

--- index.js
+++ index.js
@@ -58,13 +58,47 @@ const step0 = () => {
     return dom
   }
 
+  const isEvent = key => key.startsWith("on")
+  const isProperty = key => key !== "children" && !isEvent(key)
+  const isNew = (prev, next) => key => prev[key] !== next[key]
+  const isGone = (prev, next) => key => !(key in next)
+  function updateDom(dom, prevProps, nextProps) {
+    Object.keys(prevProps)
+      .filter(isEvent)
+      .filter(key => !(key in nextProps) || isNew(prevProps, nextProps)(key))
+      .forEach(name => {
+        const eventType = name.toLowerCase().substring(2)
+        dom.removeEventListener(
+          eventType,
+          prevProps[name]
+        )
+      })
+    // 古いプロパティを削除
+    Object.keys(prevProps)
+      .filter(isProperty)
+      .filter(isGone(prevProps, nextProps))
+      .forEach(name => {
+        dom[name] = ""
+      })
+
+      // 追加/更新されたプロパティをset
+    Object.keys(nextProps)
+      .filter(isProperty)
+      .filter(isNew(prevProps, nextProps))
+      .forEach(name => {
+        com[name] = nextprops[name]
+      })
+  }
+
   function render(element, container) {
     wipRoot = {
       dom: container,
       props: {
         children: [element]
-      }
+      },
+      alternate: currentRoot
     }
+    deletions = []
     nextUnitOfWork = wipRoot
   }
 
@@ -78,13 +112,22 @@ const step0 = () => {
       return
     }
     const domParent = fiber.parent.dom
-    domParent.appendChild(fiber.dom)
+    if (fiber.effectTag === "PLACEMENT" && fiber.dom != null) {
+      domParent.appendChild(fiber.dom)
+    } else if (fiber.effectTag === "UPDATE" && fiber.dom != null) {
+      updateDom(fiber.dom, fiber.alternate.props, fiber.props)
+    } else if (fiber.effectTag === "DELETION") {
+      domParent.removeChild(fiber.dom)
+    }
+    
     commitWork(fiber.child)
     commitWork(fiber.sibling)
   }
 
+  let currentRoot = null
   let wipRoot = null
   let nextUnitOfWork = null
+  let deletions = null
   function workLoop(deadline) {
     let shouldYield = false
     while (nextUnitOfWork && !shouldYield) {
@@ -113,34 +156,34 @@ const step0 = () => {
     // }
 
     const elements = fiber.props.children
-    let index = 0
-    // Prev sibling is elements that is rendered by prev below loop.
-    let prevSibling = null
+    // let index = 0
+    // // Prev sibling is elements that is rendered by prev below loop.
+    // let prevSibling = null
 
-    // Do for each children elements.
+    // // Do for each children elements.
     while (index < elements.length) {
-      const element = elements[index]
-      // Create new fiber 
-      // The reason is newFiber.dom is null that adove createDOM()
-      const newFiber = {
-        type: element.type,
-        props: element.props,
-        parent: fiber,
-        dom: null
-      }
+    //   const element = elements[index]
+    //   // Create new fiber 
+    //   // The reason is newFiber.dom is null that adove createDOM()
+    //   const newFiber = {
+    //     type: element.type,
+    //     props: element.props,
+    //     parent: fiber,
+    //     dom: null
+    //   }
 
-      // 最初の子要素であればchild
-      if (index === 0) {
-        fiber.child = newFiber
-      }
-      // If index !== 0 , prevSibling is not null always.
-      else {
-        prevSibling.sibling = newFiber
-      }
+    //   // 最初の子要素であればchild
+    //   if (index === 0) {
+    //     fiber.child = newFiber
+    //   }
+    //   // If index !== 0 , prevSibling is not null always.
+    //   else {
+    //     prevSibling.sibling = newFiber
+    //   }
 
       // Prevsibling should be prev element
-      prevSibling = newFiber
-      index++
+      // prevSibling = newFiber
+      // index++
 
       // 子要素、兄弟、おじの順で処理
       if (fiber.child) {
@@ -153,6 +196,73 @@ const step0 = () => {
         }
         nextFiber = nextFiber.parent
       }
+    }
+  }
+
+  function reconcileChildren(wipFiber, elements) {
+    let index = 0
+    let oldFiber = wipFiber.alternate && wipFiber.alternate.child
+    // Prev sibling is elements that is rendered by prev below loop.
+    let prevSibling = null
+
+    // Do for each children elements.
+    while (index < elements.length) {
+      const element = elements[index]
+      let newFiber = null
+      // Create new fiber 
+      // The reason is newFiber.dom is null that adove createDOM()
+      // const newFiber = {
+      //   type: element.type,
+      //   props: element.props,
+      //   parent: fiber,
+      //   dom: null
+      // }
+
+      const sameType = oldFiber && element && element.type === oldFiber.type
+
+      if (sameType) {
+        // update node
+        newFiber = {
+          type: oldFiber.type,
+          props: element.props,
+          dom: oldFiber.dom,
+          parent: wipFiber,
+          alternate: oldFiber,
+          effectTag: "UPDATE"
+        }
+      }
+
+      if (element && !sameType) {
+        newFiber = {
+          type: element.type,
+          props: element.props,
+          dom: null,
+          parent: wipFiber,
+          alternate: null,
+          effectTag: "PLACEMENT",
+        }
+      }
+
+      if (oldFiber && !sameType) {
+        oldFiber.effectTag = "DELETION"
+        deletions.push(oldFiber)
+      }
+
+      if (oldFiber) {
+        oldFiber = oldFiber.sibling
+      }
+
+      // 最初の子要素であればchild
+      if (index === 0) {
+        fiber.child = newFiber
+      }
+      // If index !== 0 , prevSibling is not null always.
+      else {
+        prevSibling.sibling = newFiber
+      }
+
+      prevSibling = newFiber
+      index++
     }
   }
 
