823ce7e874003af0d3887212b56e7034ed18bce1
sakamuuy <sakamuu.y@gmail.com>
2021-12-07 22:48:46 +0900

    step8

--- index.js
+++ index.js
@@ -213,9 +213,41 @@ const step0 = () => {
     }
   }
 
+  let wipFiber = null
+  let hookIndex = null
   function updateFunctionComponent(fiber) {
+    wipFiber = fiber
+    hookIndex = 0
+    wipFiber.hooks = []
     const children = [fiber.type(fiber.props)]
     reconcileChildren(fiber, children)
+  }
+
+  function useState(initial) {
+    const oldHook = wipFiber.alternate && wipFiber.alternate.hooks && wipFiber.alternate.hooks[hookIndex]
+    const hook = {
+      state: oldHook ? oldHook.state : initial
+    }
+
+    const setState = action => {
+      hook.queue.push(action)
+      wipRoot = {
+        dom: currentRoot.dom,
+        props: currentRoot.props,
+        alternate: currentRoot,
+      }
+      nextUnitOfWOrk = wipRoot
+      deletions = []
+    }
+
+    const actions = oldHook ? oldHook.queue : []
+    actions.forEach(action => {
+      hook.state = action(hook.state)
+    })
+
+    wipFiber.hooks.push(hook)
+    hookIndex++
+    return [hook.state]
   }
 
   function updateHostComponent(fiber) {
